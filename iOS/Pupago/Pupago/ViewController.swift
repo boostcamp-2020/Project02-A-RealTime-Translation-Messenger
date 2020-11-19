a6fb316b0ac24f52e35c654de1acd0069edcc070
kgs19970120 <rmstn9007@naver.com>
2020-11-19 18:14:10 +0900

    [iOS] Docs : :books: Init and set project #1
    - 푸파고 프로젝트 생성
    - Pod (Rxswift, RxCocoa, RxTest, RxBlocking, SwiftLint) 설정

---
+++ iOS/Pupago/Pupago/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Pupago
+//
+//  Created by 김근수 on 2020/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
