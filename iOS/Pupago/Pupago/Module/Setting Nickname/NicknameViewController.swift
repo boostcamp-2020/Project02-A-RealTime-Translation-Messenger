7c9ee00975fcaa8ce6cb90fb4e422e7651ec28d6
kimn <rile1036@naver.com>
2020-11-24 18:46:28 +0900

    [iOS] Feat : :zap:️ Implement Nickname UI Function, Regex #9
    - 닉네임 관련 규칙 Regex로 구현
    - 오류 시 붉은 색, 버튼 비활성화

--- iOS/Pupago/Pupago/Module/Setting Nickname/NicknameViewController.swift
+++ iOS/Pupago/Pupago/Module/Setting Nickname/NicknameViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NicknameViewController: ViewController {
     
@@ -16,6 +18,37 @@ class NicknameViewController: ViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindViewModel()
     }
     
+    override func bindViewModel() {
+        guard let viewModel = viewModel as? NicknameViewModel else { return }
+        let nameText = nameTextField.rx.text.asObservable()
+        
+        let input = NicknameViewModel.Input(nicknameText: nameText)
+        let output = viewModel.transform(input)
+        
+        output.viewTexts
+            .drive(onNext: { [weak self] texts in
+                self?.introLabel.text = texts.intro
+                self?.descriptionLabel.text = texts.inputConstraint
+                self?.nameTextField.placeholder = texts.inputPlaceholder
+                self?.startButton.setTitle(texts.nextButton, for: .normal)
+            })
+            .disposed(by: rx.disposeBag)
+        
+        output.isNicknameValid
+            .drive(onNext: { [weak self] isValid in
+                self?.nameTextField.isValid = isValid
+                self?.descriptionLabel.textColor = isValid ? .white : .red
+            })
+            .disposed(by: rx.disposeBag)
+        
+        output.activate
+            .drive(onNext: { [weak self] activate in
+                self?.startButton.isUserInteractionEnabled = activate
+                self?.startButton.backgroundColor = activate ? UIColor(named: "ButtonColor") : .systemGray6
+            })
+            .disposed(by: rx.disposeBag)
+    }
 }
--- iOS/Pupago/Pupago/Module/Setting Nickname/NicknameViewModel.swift
+++ iOS/Pupago/Pupago/Module/Setting Nickname/NicknameViewModel.swift
@@ -6,18 +6,55 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class NicknameViewModel: ViewModel, ViewModelType {
     
     struct Input {
-        
+        let nicknameText: Observable<String?>
     }
     
     struct Output {
-        
+        let viewTexts: Driver<Localize.SettingNicknameViewText>
+        let isNicknameValid: Driver<Bool>
+        let activate: Driver<Bool>
     }
     
+    let isEmpty = BehaviorRelay<Bool>(value: true)
+    let isValid = BehaviorRelay<Bool>(value: false)
+    
     func transform(_ input: Input) -> Output {
-        return Output()
+        input.nicknameText
+            .map { $0?.isEmpty ?? true }
+            .bind(to: isEmpty)
+            .disposed(by: rx.disposeBag)
+        
+        input.nicknameText
+            .map { self.validate(nickname: $0 ?? "") }
+            .bind(to: isValid)
+            .disposed(by: rx.disposeBag)
+        
+        let viewText = localize.asDriver()
+            .map { $0.nicknameViewText }
+        
+        let isNicknameValid = Observable.zip(isEmpty, isValid)
+            .map { $0 || $1 }
+            .asDriver(onErrorJustReturn: false)
+        
+        let activate = Observable.zip(isEmpty, isValid)
+            .map { !$0 && $1 }
+            .asDriver(onErrorJustReturn: false)
+        
+        return Output(viewTexts: viewText,
+                      isNicknameValid: isNicknameValid,
+                      activate: activate)
+    }
+    
+    private func validate(nickname: String) -> Bool {
+        guard nickname.count <= 12 && nickname.count >= 2 else { return false }
+        guard getSyntaxAfterRegex(newText: nickname) else { return false }
+        
+        return true
     }
 }
