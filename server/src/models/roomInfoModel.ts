be842ace368d3ea80884389899338d95f485e9d0
SoHyun <pje1740@gmail.com>
2020-11-26 11:10:40 +0900

    [Web] Refactor: :rainbow: combine model functions into one Model instance  #40
    모델 관련 함수를 하나의 객체에 묶어서 export 하는 형태로 수정.

--- server/src/models/roomInfoModel.ts
+++ server/src/models/roomInfoModel.ts
@@ -2,4 +2,10 @@ import client from './redisConnection';
 import Database from '../types/databaseNames';
 
 // 방 삭제하기 (key 삭제)
-const removeRoom = (roomId: string) => {};
+const removeRoom = (roomCode: string) => {};
+
+const roomInfoModel = {
+  removeRoom,
+};
+
+export default roomInfoModel;
--- server/src/socket/socketController.ts
+++ server/src/socket/socketController.ts
@@ -2,15 +2,15 @@ import { userDataType, participantsListType, sendChatType, receiveChatType } fro
 import SocketIO from 'socket.io';
 
 const enterChatroom = (socket: any, io: SocketIO.Server, userData: userDataType) => {
-  const { nickname, language, roomId } = userData;
-  socket.join(roomId);
+  const { roomCode, nickname, language } = userData;
+  socket.join(roomCode);
   // TODO: db1, 2 소켓 정보 저장
   const participantsList: participantsListType = {
     // TODO: 참가자 리스트 가져와서 넣어주기
     participantsList: [],
     type: 'enter',
   };
-  io.to(roomId).emit('receive participants list', participantsList);
+  io.to(roomCode).emit('receive participants list', participantsList);
 };
 
 const sendChat = (socket: any, io: SocketIO.Server, sendChat: sendChatType) => {
@@ -21,19 +21,19 @@ const sendChat = (socket: any, io: SocketIO.Server, sendChat: sendChatType) => {
     senderId: socket.id,
   };
   // TODO: 현재 소켓이 속해있는 방 id 받아오기
-  const roomId = '1'; // db를 통해 받아오기
-  io.to(roomId).emit('receive chat', receiveChat);
+  const roomCode = '1'; // db를 통해 받아오기
+  io.to(roomCode).emit('receive chat', receiveChat);
 };
 
 const disconnect = (socket: any, io: SocketIO.Server) => {
   // TODO: 현재 소켓이 속해있는 방 id 받아오기
-  const roomId = '1'; // db를 통해 받아오기
+  const roomCode = '1'; // db를 통해 받아오기
   const participantsList: participantsListType = {
     // TODO: 참가자 리스트 가져와서 넣어주기
     participantsList: [],
     type: 'leave',
   };
-  io.to(roomId).emit('receive participants list', participantsList);
+  io.to(roomCode).emit('receive participants list', participantsList);
   // TODO: 소켓을 DB 1, 2번에서 제거
   // TODO: 방이 비었는지 확인하고 비었으면 0번에서 해당 방 제거
 };
