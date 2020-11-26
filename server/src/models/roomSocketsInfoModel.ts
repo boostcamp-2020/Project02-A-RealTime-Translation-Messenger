63662a2b091b7e494b6f68f856f4ddf02bda2c2a
SoHyun <pje1740@gmail.com>
2020-11-26 13:37:54 +0900

    [Web] Feat : :zap:️ implement setSocketInfo function  #45
    roomSocketsInfoModel 의 setSocketInfo를 구현.

--- server/src/models/roomSocketsInfoModel.ts
+++ server/src/models/roomSocketsInfoModel.ts
@@ -2,7 +2,16 @@ import client from './redisConnection';
 import Database from '../types/databaseNames';
 
 // 소켓 정보 저장 (룸의 소켓 리스트에 소켓 추가)
-const setSocketInfo = (socketId: string, socketInfo: string) => {};
+const setSocketInfo = (roomCode: string, socketId: string, socketInfo: string) => {
+  return new Promise((resolve, reject) => {
+    client.select(Database.ROOM_SOCKETS_INFO, () => {
+      client.hset([roomCode, socketId, socketInfo], (err, res) => {
+        if (err) reject(err);
+        return resolve(res);
+      });
+    });
+  });
+};
 
 // 룸에 속한 소켓 리스트 뽑아주기
 const getSocketsByRoom = (roomCode: string) => {};
