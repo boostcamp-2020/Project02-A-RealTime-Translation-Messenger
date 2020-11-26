3dd7c5ee51b539d2631eaff50275032712a471e2
kimn <rile1036@naver.com>
2020-11-26 11:25:20 +0900

    [iOS] Feat : :zap:️ Implement Cancle Button #18
    - 취소 버튼 클릭 시, 아무런 변화 없이 dismiss 기능 추가
    - ViewController 위치 변경

--- iOS/Pupago/Pupago/Module/Create Chatroom/CreateRoomViewModel.swift
+++ iOS/Pupago/Pupago/Module/Create Chatroom/CreateRoomViewModel.swift
@@ -6,15 +6,24 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class CreateRoomViewModel: ViewModel, ViewModelType {
     struct Input {
+        let closeRoomTrigger: Observable<Void>
     }
     
     struct Output {
+        let dismiss: Driver<Void>
     }
     
     func transform(_ input: Input) -> Output {
-        return Output()
+        
+        let closeSelected = input.closeRoomTrigger
+            .map { _ in }
+            .asDriver(onErrorJustReturn: ())
+        
+        return Output(dismiss: closeSelected)
     }
 }
