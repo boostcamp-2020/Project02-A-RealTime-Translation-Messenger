1bd1a77fcf1d4e11425379197f0fc01b0ea874dd
moaikang <kk_w1207@naver.com>
2020-11-29 19:52:26 +0900

    [Web] Refactor: :rainbow: seperate roomCode validation logic from routes  #61
    roomCode를 검증하는 로직을 utils로 분리하였다.

--- server/src/routes/join.ts
+++ server/src/routes/join.ts
@@ -1,32 +1,30 @@
 import express, { Request, Response } from 'express';
 import roomInfoModel from '../models/roomInfoModel';
 import { CreatedRoomType } from '../types/socketTypes';
+import { isRoomCodeValidate } from '../utils/validate';
 
 const router = express.Router();
 
 router.post('/private', async (req: Request, res: Response) => {
-  const roomCode = req.body.roomCode;
+  const { roomCode } = req.body;
 
-  if (/[A-Z0-9]{4}/.test(roomCode)) {
-    if ((await roomInfoModel.isRoomCodeExisting(roomCode)) && (await roomInfoModel.isRoomPrivate(roomCode))) {
-      const title: string = await roomInfoModel.getTitle(roomCode);
-      const createdRoom: CreatedRoomType = { roomCode, title, isPrivate: 'true' };
-      return res.status(200).json(createdRoom);
-    }
+  if (await isRoomCodeValidate(roomCode, 'private')) {
+    const title = await roomInfoModel.getTitle(roomCode);
+    const createdRoom: CreatedRoomType = { roomCode, title, isPrivate: 'true' };
+    return res.status(200).json(createdRoom);
   }
   return res.status(400).json();
 });
 
 router.post('/public', async (req: Request, res: Response) => {
-  const roomCode = req.body.roomCode;
+  const { roomCode } = req.body;
 
-  if (/[A-Z0-9]{4}/.test(roomCode)) {
-    if ((await roomInfoModel.isRoomCodeExisting(roomCode)) && !(await roomInfoModel.isRoomPrivate(roomCode))) {
-      const title: string = await roomInfoModel.getTitle(roomCode);
-      const createdRoom: CreatedRoomType = { roomCode, title, isPrivate: 'false' };
-      return res.status(200).json(createdRoom);
-    }
+  if (await isRoomCodeValidate(roomCode, 'public')) {
+    const title = await roomInfoModel.getTitle(roomCode);
+    const createdRoom: CreatedRoomType = { roomCode, title, isPrivate: 'false' };
+    return res.status(200).json(createdRoom);
   }
+
   return res.status(400).json();
 });
 
---
+++ server/src/utils/validate.ts
@@ -0,0 +1,18 @@
+import roomInfoModel from '../models/roomInfoModel';
+
+export const isRoomCodeValidate = async (roomCode: string, isPrivate: string) => {
+  if (!/[A-Z0-9]{4}/.test(roomCode)) return false;
+  if (!(await roomInfoModel.isRoomCodeExisting(roomCode))) return false;
+
+  if (isPrivate === 'private') {
+    if (!(await roomInfoModel.isRoomPrivate(roomCode))) {
+      return false;
+    }
+    return true;
+  }
+
+  if (await roomInfoModel.isRoomPrivate(roomCode)) {
+    return false;
+  }
+  return true;
+};
