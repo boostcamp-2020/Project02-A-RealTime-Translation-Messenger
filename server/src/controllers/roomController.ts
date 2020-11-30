f93c9edc130c69c011ca90e52ad4ee4e7594c82e
kas136 <kas136@naver.com>
2020-11-30 22:28:34 +0900

    [Web] Feat : :zap:️ add validation to room api #61
    room api에 room title, isPrivate에 대한 유효성 검사 추가

--- server/src/controllers/roomController.ts
+++ server/src/controllers/roomController.ts
@@ -4,7 +4,8 @@ import roomInfoModel from '../models/roomInfoModel';
 import roomCodeUtil from '../utils/roomCode';
 import StatusCode from '../@types/statusCode';
 import roomService from '../services/roomService';
-import { CreatedRoomType } from '../@types/socketType';
+import validationUtil from '../utils/validation';
+import { CreatedRoomType } from '../@types/dataType';
 
 const getPublicRoomList = async (req: Request, res: Response) => {
   try {
@@ -14,12 +15,14 @@ const getPublicRoomList = async (req: Request, res: Response) => {
 
     return res.status(StatusCode.OK).json({ roomList: filteredRoomLists });
   } catch (err) {
-    return res.status(StatusCode.CLIENT_ERROR).json();
+    return res.status(StatusCode.SERVER_ERROR).json();
   }
 };
 
 const createRoom = async (req: Request, res: Response) => {
   const { title, isPrivate } = req.body;
+  if (!(validationUtil.isTitleValid(title) && validationUtil.isIsPrivateValid(isPrivate)))
+    return res.status(StatusCode.CLIENT_ERROR).json();
 
   try {
     const roomCode = await roomCodeUtil.getRandomCode();
@@ -27,7 +30,7 @@ const createRoom = async (req: Request, res: Response) => {
     const createdRoom: CreatedRoomType = { roomCode, title, isPrivate };
     return res.status(StatusCode.OK).json(createdRoom);
   } catch (err) {
-    return res.status(StatusCode.CLIENT_ERROR).json();
+    return res.status(StatusCode.SERVER_ERROR).json();
   }
 };
 
