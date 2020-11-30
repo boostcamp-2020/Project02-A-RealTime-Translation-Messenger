ba73a809d561fe1ddc01cf78b30e6259e4e6197f
kimn <rile1036@naver.com>
2020-11-30 11:28:36 +0900

    [iOS] Feat : :zap:️ Implement Chatting UI #63
    - 유저들간의 채팅 UI 디자인
    - 멀티 셀로 각각 나눠 설정

--- iOS/Pupago/Pupago/Module/Chatting/ChattingViewController.swift
+++ iOS/Pupago/Pupago/Module/Chatting/ChattingViewController.swift
@@ -6,13 +6,54 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxKeyboard
+import RxDataSources
 
 class ChattingViewController: ViewController {
 
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var inputText: UITextView!
+    @IBOutlet weak var registButton: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Test UI
-        self.navigationItem.title = "푸파고"
-        self.navigationController?.navigationBar.tintColor = UIColor.white
+        configureCollectionView()
+    }
+    
+    override func bindViewModel() {
+        super.bindViewModel()
+        
+        guard let viewModel = viewModel as? ChattingViewModel else { return }
+        
+        let chatText = inputText.rx.text.asObservable()
+        let registTrigger = registButton.rx.tap.map { _ in }
+        
+        let input = ChattingViewModel.Input(chatText: chatText,
+                                        registTrigger: registTrigger)
+        
+        let output = viewModel.transform(input)
+        
+    }
+}
+
+extension ChattingViewController {
+    func configureCollectionView() {
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                          heightDimension: .estimated(33))
+        let item = NSCollectionLayoutItem(layoutSize: size)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitem: item, count: 1)
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10.0
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        self.collectionView.collectionViewLayout = layout
+        self.collectionView.alwaysBounceVertical = true
+        self.collectionView.keyboardDismissMode = .interactive
     }
 }
--- iOS/Pupago/Pupago/Module/Chatting/ChattingViewModel.swift
+++ iOS/Pupago/Pupago/Module/Chatting/ChattingViewModel.swift
@@ -6,14 +6,38 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 class ChattingViewModel: ViewModel, ViewModelType {
     
-    struct Input {}
-    struct Output {}
+    struct Input {
+        let chatText: Observable<String?>
+        let registTrigger: Observable<Void>
+    }
+    struct Output {
+        let items: Driver<[MessageSection]>
+    }
+    
+    let chats = BehaviorRelay<[MessageSection]>(value: [MessageSection(header: "Chat", items: [Message(user: .others, messageItems: MessageItem(text: "testtest", chattingAt: ""))
+    ])])
     
     func transform(_ input: Input) -> Output {
-        return Output()
+        
+        input.registTrigger
+            .withLatestFrom(input.chatText)
+            .subscribe(onNext: { text in
+                
+                self.chats.accept(self.chats.value +
+                                    [MessageSection(header: "Chat", items: [Message(user: .mine, messageItems: MessageItem(text: "testtest", chattingAt: ""))
+                                    ])])
+            })
+            .disposed(by: rx.disposeBag)
+        
+        let chatItem = chats.asDriver(onErrorJustReturn: [])
+        
+        return Output(items: chatItem)
     }
     
 }
