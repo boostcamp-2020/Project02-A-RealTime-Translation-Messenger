25704f19f9376f2b9df2953a32e232a1c5d96c3e
SoHyun <pje1740@gmail.com>
2020-11-30 18:47:10 +0900

    [Web] Feat : :zap:️ change socket return value type to JSON string #79
    iOS와의 통신을 위해 소켓 통신 시 반환하는 값을 모두 JSON으로 변경.

--- server/src/sockets/socketController.ts
+++ server/src/sockets/socketController.ts
@@ -11,13 +11,13 @@ const enterChatroom = async (socket: Socket, io: SocketIO.Server, userData: User
   socket.join(roomCode);
   await socketService.insertSocketInfoIntoDB(socket.id, roomCode, nickname, language);
   const participantsList = await socketService.getParticipantsListFromRoomCode(roomCode, 'enter');
-  io.to(roomCode).emit('receive participants list', participantsList);
+  io.to(roomCode).emit('receive participants list', JSON.stringify(participantsList));
 };
 
 const sendChat = async (socket: Socket, io: SocketIO.Server, sendChat: SendChatType) => {
   const { Korean, English } = sendChat;
   const { roomCode, receiveChat } = await socketService.createReceiveChatType(socket.id, Korean, English);
-  io.to(roomCode).emit('receive chat', receiveChat);
+  io.to(roomCode).emit('receive chat', JSON.stringify(receiveChat));
 };
 
 const disconnect = async (socket: Socket, io: SocketIO.Server) => {
@@ -30,7 +30,7 @@ const disconnect = async (socket: Socket, io: SocketIO.Server) => {
   }
 
   const participantsList = await socketService.getParticipantsListFromRoomCode(roomCode, 'leave');
-  io.to(roomCode).emit('receive participants list', participantsList);
+  io.to(roomCode).emit('receive participants list', JSON.stringify(participantsList));
 };
 
 const socketController = {
