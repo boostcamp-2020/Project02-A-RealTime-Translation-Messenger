f52d66e540d861609874a2f06cd507f31b9f153f
kgs19970120 <rmstn9007@naver.com>
2020-12-02 15:40:27 +0900

    [iOS] Feat : :zap:️Implement SocketIOManager, SocketEndpoint #73
    - 소캣 연결 및 사용을 위한 Manager 를 singleton 객체로 구현
    - 소캣 이벤트 및 url을 관리하기 위한 Endpoint 작성

--- iOS/Pupago/Pupago/Network/Endpoint/SocketEndpoint.swift
+++ iOS/Pupago/Pupago/Network/Endpoint/SocketEndpoint.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+protocol SocketEndpointType {
+    var baseUrl: URL { get }
+    var eventName: String { get }
+}
+
+enum SocketEndpoint {
+    case connect
+    case enter
+    case leave
+    case sendMessage
+    case receiveMessage
+    case list
+    case error
+}
+
+extension SocketEndpoint: SocketEndpointType {
+    var baseUrl: URL {
+        let urlString = "192.0.0.1:3000"
+        let url = URL(string: urlString) ?? URL(fileURLWithPath: "")
+        return url
+    }
+    
+    var eventName: String {
+        switch self {
+        case .connect:
+            return "connect"
+        case .enter:
+            return "enter chatroom"
+        case .leave:
+            return "leave chatroom"
+        case .sendMessage:
+            return "send chat"
+        case .receiveMessage:
+            return "receive chat"
+        case .list:
+            return "receive participants list"
+        case .error:
+            return "socketError"
+        }
+    }
+    
+    
+}
--- iOS/Pupago/Pupago/Network/SocketManager.swift
+++ iOS/Pupago/Pupago/Network/SocketManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SocketIO
+
+class SocketIOManager {
+    
+    static let shared = SocketIOManager()
+    let manager = SocketManager(socketURL: URL(string: "ws://118.67.134.11:3000")!, config: [.log(false), .forceWebsockets(true), .compress])
+    var socket: SocketIOClient!
+    
+    private init() {
+        socket = manager.defaultSocket
+    }
+    
+    func establishConnect() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+}
