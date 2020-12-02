4fc7b4cabde44b9368ccaa50258c16bf0901e05e
SoHyun <pje1740@gmail.com>
2020-12-02 18:52:36 +0900

    [Web] Feat : :zap:️ create CryingPapago component and stories #131

---
+++ client/src/components/atoms/CryingPapago.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Logo = styled.img`
+  display: block;
+  width: 115px;
+  height: 144px;
+`;
+
+export function CryingPapago() {
+  return <Logo src="https://i.imgur.com/SH66ghB.png" alt="crying papago" />;
+}
+
+export default CryingPapago;
