bf55927ce8116dab5038c0c3d0a53dfa6702ff10
SoHyun <pje1740@gmail.com>
2020-12-02 22:40:30 +0900

    [Web] Feat : :zap:️ create IconButton component and stories #128
    각각의 아이콘을 입력받은 문자열에 따라 보여주는 컴포넌트 생성

---
+++ client/src/components/atoms/IconButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import EditIcon from '@material-ui/icons/Edit';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import SendIcon from '@material-ui/icons/Send';
+import MicIcon from '@material-ui/icons/Mic';
+import LeaveIcon from '@material-ui/icons/ExitToApp';
+
+export type IconButtonPropsType = {
+  iconType: string;
+  color: string;
+};
+
+export function IconButton({ iconType = 'Edit', color = 'black' }: IconButtonPropsType) {
+  let icon;
+  if (iconType === 'Edit') icon = <EditIcon style={{ fontSize: 16, color: color }} />;
+  else if (iconType === 'ArrowBack') icon = <ArrowBackIcon style={{ fontSize: 16, color: color }} />;
+  else if (iconType === 'Send') icon = <SendIcon style={{ fontSize: 16, color: color }} />;
+  else if (iconType === 'Mic') icon = <MicIcon style={{ fontSize: 16, color: color }} />;
+  else if (iconType === 'Leave') icon = <LeaveIcon style={{ fontSize: 16, color: color }} />;
+
+  return <>{icon}</>;
+}
+
+export default IconButton;
