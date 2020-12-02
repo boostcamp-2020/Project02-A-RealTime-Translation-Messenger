6bfd7cce757edb1755b95756e01ce34376aa6cfb
SoHyun <pje1740@gmail.com>
2020-12-02 15:56:23 +0900

    [Web] Fix : :fire: fix css order, display option, color enum  #102
    css 작성 순서 통일 및 버튼 블록 옵션, 색상 enum 수정 등

--- client/src/components/MainButton.tsx
+++ client/src/components/MainButton.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import Palette from '../@types/Palette';
 
-export interface ButtonPropsType {
+export type ButtonPropsType = {
   disabled?: boolean;
   onClick?: () => void;
-}
+};
 
 const Button = styled.button<ButtonPropsType>`
+  display: block;
   width: 336px;
   height: 40px;
+  filter: drop-shadow(3px 3px 10px rgba(0, 0, 0, 0.25));
   border-radius: 10px;
   border: none;
-  color: #808080;
+  background-color: ${(props) => (props.disabled ? Palette.DISABLED_YELLOW : Palette.ACTIVE_YELLOW)};
+  color: ${Palette.DARK_GREY};
   font-size: 18px;
-  background-color: ${(props) => (props.disabled ? '#d6b400' : '#ffd700')};
-  filter: drop-shadow(3px 3px 10px rgba(0, 0, 0, 0.25));
 `;
 
 export function MainButton({ disabled = true, children = 'button', ...props }) {
   return (
-    <div>
-      <Button type="button" disabled={disabled}>
-        {children}
-      </Button>
-    </div>
+    <Button type="button" disabled={disabled}>
+      {children}
+    </Button>
   );
 }
 
