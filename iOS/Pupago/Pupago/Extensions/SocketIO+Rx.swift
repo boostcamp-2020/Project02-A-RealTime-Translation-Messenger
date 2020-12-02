4fc154cc83498e1133b699c04f3f67cbac3032f3
kgs19970120 <rmstn9007@naver.com>
2020-12-02 15:38:13 +0900

    [iOS] Feat : :zap:️ Implement socketio for rx extension #73
    - Socket io 이벤트를 rx로 감싸서 처리하기 위한 extension 구현

--- iOS/Pupago/Pupago/Extensions/SocketIO+Rx.swift
+++ iOS/Pupago/Pupago/Extensions/SocketIO+Rx.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+import RxSwift
+import SocketIO
+
+extension Reactive where Base: SocketIOClient {
+    
+    func event(_ endpoint: SocketEndpoint) -> Observable<SocketAnyEvent> {
+        return Observable.create { observer in
+            self.base.onAny { event in
+                if event.event == endpoint.eventName {
+                    observer.onNext(event)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+}
