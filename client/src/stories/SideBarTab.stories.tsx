22a14feb7112d626a005cb2dba10d49b7ad4d58b
moaikang <kk_w1207@naver.com>
2020-12-02 22:37:42 +0900

    [Web] Feat : :zap:️ Create SideBarTab Component's story #123
    SideBarTab 컴포넌트의 스토리를 만들었다.

---
+++ client/src/stories/SideBarTab.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import SideBarTab, { SideBarTabPropsType } from '../components/atoms/SideBarTab';
+
+export default {
+  title: 'pupago/SideBarTab',
+  component: SideBarTab,
+} as Meta;
+
+const Template: Story<SideBarTabPropsType> = (args) => <SideBarTab {...args} />;
+
+export const ActiveParticipant = Template.bind({});
+ActiveParticipant.args = {
+  children: '참여자',
+  isSelected: true,
+  isTabNameParticipant: true,
+};
+
+export const DisabledParticipant = Template.bind({});
+DisabledParticipant.args = {
+  children: '참여자',
+  isSelected: false,
+  isTabNameParticipant: true,
+};
+
+export const ActiveChatRoom = Template.bind({});
+ActiveChatRoom.args = {
+  children: '채팅방',
+  isSelected: true,
+  isTabNameParticipant: false,
+};
+
+export const DisabledChatRoom = Template.bind({});
+DisabledChatRoom.args = {
+  children: '채팅방',
+  isSelected: false,
+  isTabNameParticipant: false,
+};
