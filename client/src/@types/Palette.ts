82e8dabb4f9498a739afaef9d12ba1d116f0324b
moaikang <kk_w1207@naver.com>
2020-12-02 21:19:10 +0900

    [Web] Feat : :zap:️ make ChatBox Component #126
    ChatBox Component를 만들었다.

--- client/src/@types/Palette.ts
+++ client/src/@types/Palette.ts
@@ -2,6 +2,8 @@ enum Palette {
   DARK_GREY = '#808080',
   ACTIVE_YELLOW = '#ffd700',
   DISABLED_YELLOW = '#d6b400',
+  PUPAGO_BLUE = '#5ca7e4',
+  LIGHT_GREY = '#e5e5e5',
 }
 
 export default Palette;
