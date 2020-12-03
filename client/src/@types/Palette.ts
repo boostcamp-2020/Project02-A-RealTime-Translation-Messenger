f7e735c38fb26b2fc97ced4d05c49deb46b98598
moaikang <kk_w1207@naver.com>
2020-12-03 15:45:48 +0900

    [Web] Feat : :zap:️ Create ChatInput component #130
    ChatInput component를 만들었다.

--- client/src/@types/Palette.ts
+++ client/src/@types/Palette.ts
@@ -2,6 +2,7 @@ enum Palette {
   DARK_GREY = '#808080',
   ACTIVE_YELLOW = '#ffd700',
   DISABLED_YELLOW = '#d6b400',
+  ALMOST_WHITE = '#f8f9fd;',
 }
 
 export default Palette;
---
+++ client/src/components/atoms/ChatInput.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import Palette from '../../@types/Palette';
+
+const StyledChatInput = styled.textarea`
+  display: block;
+
+  padding: 25px;
+
+  width: 476px;
+  height: 96px;
+
+  outline: none;
+  border: none;
+  border-radius: 30px 0 0 0;
+
+  box-sizing: border-box;
+
+  background-color: ${Palette.ALMOST_WHITE};
+
+  resize: none;
+  overflow: hidden;
+
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export type ChatInputPropsType = {
+  value: string;
+  onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+};
+
+function ChatInput({ value, onChange }: ChatInputPropsType) {
+  return <StyledChatInput onChange={onChange} value={value} />;
+}
+
+export default ChatInput;
