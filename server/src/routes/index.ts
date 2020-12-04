722966613137b3d40b3e3b21acb09cf42d83d859
SoHyun <pje1740@gmail.com>
2020-12-04 17:17:05 +0900

    [Web] Feat : :zap:️ create router that return random profile image link #167
    랜덤 이미지 링크를 반환하는 기능 구현.
    컨트롤러와 서비스로 분리 필요

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import roomRouter from './room';
 import joinRouter from './join';
+import profileImageRouter from './profileImage';
 
 const router = express.Router();
 
 router.use('/room', roomRouter);
 router.use('/join', joinRouter);
+router.use('/profileImage', profileImageRouter);
 
 export default router;
