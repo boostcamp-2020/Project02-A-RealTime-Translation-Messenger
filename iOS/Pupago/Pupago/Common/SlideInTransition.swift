ac099166a0045362d74a9d04e3dd9e89f5a7ce2d
kgs19970120 <rmstn9007@naver.com>
2020-12-05 01:52:23 +0900

    [iOS] Feat : :zap:️Implement slide in transition logic, helper #189

--- iOS/Pupago/Pupago/Common/SlideInTransition.swift
+++ iOS/Pupago/Pupago/Common/SlideInTransition.swift
@@ -5,4 +5,56 @@
 //  Created by 김근수 on 2020/12/05.
 //
 
-import Foundation
+import UIKit
+
+class SlideInTransition: NSObject, UIViewControllerAnimatedTransitioning {
+
+    var isPresenting = false
+    let dimmingView = UIView()
+
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+
+        guard let toViewController = transitionContext.viewController(forKey: .to),
+            let fromViewController = transitionContext.viewController(forKey: .from) else { return }
+
+        let containerView = transitionContext.containerView
+
+        let viewWidth = toViewController.view.bounds.width
+        let finalWidth = viewWidth * 0.7
+        let finalHeight = toViewController.view.bounds.height
+
+        if isPresenting {
+            dimmingView.backgroundColor = .black
+            dimmingView.alpha = 0.0
+            containerView.addSubview(dimmingView)
+            dimmingView.frame = containerView.bounds
+            containerView.addSubview(toViewController.view)
+
+            toViewController.view.frame = CGRect(x: viewWidth, y: 0, width: finalWidth, height: finalHeight)
+        }
+
+        let transform = {
+            self.dimmingView.alpha = 0.5
+            toViewController.view.transform = CGAffineTransform(translationX: -finalWidth, y: 0)
+        }
+
+        let identity = {
+            self.dimmingView.alpha = 0.0
+            fromViewController.view.transform = .identity
+        }
+
+        let duration = transitionDuration(using: transitionContext)
+        let isCancelled = transitionContext.transitionWasCancelled
+        
+        UIView.animate(withDuration: duration, animations: {
+            self.isPresenting ? transform() : identity()
+        }, completion: { _ in
+            transitionContext.completeTransition(!isCancelled)
+        })
+    }
+
+}
--- iOS/Pupago/Pupago/Util/TransitionHelper.swift
+++ iOS/Pupago/Pupago/Util/TransitionHelper.swift
@@ -5,4 +5,20 @@
 //  Created by 김근수 on 2020/12/05.
 //
 
-import Foundation
+import UIKit
+
+class SlidInTransitionHelper: NSObject, UIViewControllerTransitioningDelegate {
+    
+    let transition = SlideInTransition()
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.isPresenting = true
+        return transition
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.isPresenting = false
+        return transition
+    }
+    
+}
