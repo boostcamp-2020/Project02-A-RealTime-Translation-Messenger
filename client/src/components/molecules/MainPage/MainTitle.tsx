7c7f095b449a3d363324c166d7a860da16c1ac0a
SoHyun <pje1740@gmail.com>
2020-12-05 19:55:42 +0900

    [Web] Feat : :zap:️ add MainTitle component and stories #168

---
+++ client/src/components/molecules/MainPage/MainTitle.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import LogoWithText from '../../atoms/logos/LogoWithText';
+import Text from '../../atoms/texts/Text';
+import Palette from '../../../@types/Palette';
+
+const MainTitleWrapper = styled.div`
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    margin-bottom: 16px;
+  }
+`;
+
+export const MainTitle = () => {
+  return (
+    <MainTitleWrapper>
+      <LogoWithText />
+      <Text size={24} weight={'bold'} color={Palette.PUPAGO_BLUE}>
+        실시간 번역 채팅 푸파고!
+      </Text>
+      <Text size={18} color={Palette.DARK_GREY}>
+        글로벌 채팅을 즐겨보세요!
+      </Text>
+    </MainTitleWrapper>
+  );
+};
+
+export default MainTitle;
