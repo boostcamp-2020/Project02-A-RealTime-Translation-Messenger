7cebb0313014d83f330936a0b0ea90edb5a12da9
kgs19970120 <rmstn9007@naver.com>
2020-12-05 12:32:54 +0900

    [iOS] Feat : :zap: Implement getting participants list logic at viewmodel, bind to view #190

--- iOS/Pupago/Pupago/Module/Participant/ParticipantViewModel.swift
+++ iOS/Pupago/Pupago/Module/Participant/ParticipantViewModel.swift
@@ -6,14 +6,42 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class ParticipantViewModel: ViewModel, ViewModelType {
     
-    struct Input {}
-    struct Output {}
+    struct Input {
+        let viewWillAppear: Observable<Void>
+        let dismissTrigger: Observable<Void>
+    }
+    struct Output {
+        let item: Observable<[Participant]>
+        let dismiss: Driver<Void>
+    }
+    
+    init(roomCode: String) {
+        self.roomCode = roomCode
+    }
+    
+    let roomCode: String
+    let participants = BehaviorRelay<[Participant]>(value: [])
     
     func transform(_ input: Input) -> Output {
-        return Output()
+        let provider = PupagoAPI()
+        
+        input.viewWillAppear
+            .flatMap { [unowned self] in provider.participantList(roomCode: roomCode) }
+            .subscribe(onNext: { [unowned self] result in
+                participants.accept(result.participantsList)
+            })
+            .disposed(by: rx.disposeBag)
+        
+        let item = participants.asObservable()
+        let dismiss = input.dismissTrigger.asDriver(onErrorJustReturn: ())
+        
+        return Output(item: item,
+                      dismiss: dismiss)
     }
     
 }
