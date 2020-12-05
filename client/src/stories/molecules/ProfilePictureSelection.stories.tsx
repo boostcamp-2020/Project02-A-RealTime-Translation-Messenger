e3a02bfc994e1ac3ca2adbda1bca2a669e26f93b
kas136 <kas136@naver.com>
2020-12-05 20:53:29 +0900

    [Web] Feat : :zap:️ create ProfilePictureSelection molecule and storybook #169
    ProfilePictureSelection molecule 과 storybook 생성

---
+++ client/src/stories/molecules/ProfilePictureSelection.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import ProfilePictureSelection, {
+  ProfilePictureSelectionPropsType,
+} from '../../components/molecules/mainPage/ProfilePictureSelection';
+
+export default {
+  title: 'pupagoMolecule/ProfilePictureSelection',
+  component: ProfilePictureSelection,
+} as Meta;
+
+const Template: Story<ProfilePictureSelectionPropsType> = (args) => <ProfilePictureSelection {...args} />;
+
+export const Active = Template.bind({});
+Active.args = {
+  image: `https://i.imgur.com/O3G3HwR.jpg`,
+};
