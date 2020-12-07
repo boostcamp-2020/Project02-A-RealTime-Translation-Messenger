1f4ff68b29655df55a52afffcf945bae6c633273
moaikang <kk_w1207@naver.com>
2020-12-07 16:54:32 +0900

    [Web] Chore : :package: init Redux #229
    Redux, Redux-Thunk를 설치하고, redux를 프로젝트와 연결하였다.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import App from './App';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(Thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root'),
+);
