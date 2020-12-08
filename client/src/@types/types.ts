2558d8d4f3f6df3d276badf7150b93192e8eda61
SoHyun <pje1740@gmail.com>
2020-12-08 14:42:00 +0900

    [Web] Feat : :zap:️ add ChatPage component and socket connection & events #242
    소켓이 최초 접속되는 컴포넌트인 ChatPage 컴포넌트 생성
    소켓 최초 접속과 함께 필요한 이벤트 핸들러 부착

--- client/src/@types/types.ts
+++ client/src/@types/types.ts
@@ -1,7 +1,30 @@
-export type RoomListType = {
+type RoomListType = {
   roomCode: string;
   title: string;
   createdAt: string;
   participantCount: number;
   isPrivate: string;
 };
+
+type ParticipantsType = {
+  socketId: string;
+  nickname: string;
+  language: string;
+};
+
+type ParticipantsListType = {
+  participantsList: ParticipantsType[];
+  type: string;
+  diffNickname: string;
+};
+
+type ReceiveChatType = {
+  Korean: string;
+  English: string;
+  senderId: string;
+  nickname: string;
+  imageLink: string;
+  createdAt: string;
+};
+
+export { RoomListType, ParticipantsType, ParticipantsListType, ReceiveChatType };
