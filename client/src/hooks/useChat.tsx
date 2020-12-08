d0b247ac2da3510078e499b8df7083160187a4f3
kas136 <kas136@naver.com>
2020-12-08 23:08:35 +0900

    [Web] Feat : :zap:️ add onStackChats custom hook #252
    onStackChats custom hook 생성

---
+++ client/src/hooks/useChat.tsx
@@ -0,0 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../modules';
+import { stackChats } from '../modules/chat';
+import { useCallback } from 'react';
+
+import { ChatLogsType } from '../@types/types';
+
+export default function useChat() {
+  const { data } = useSelector((state: RootState) => state.chat.chatLogs);
+  const dispatch = useDispatch();
+
+  const onStackChats = useCallback((chatLogs: ChatLogsType[]) => dispatch(stackChats(chatLogs)), [dispatch]);
+
+  return {
+    data,
+    onStackChats,
+  };
+}
