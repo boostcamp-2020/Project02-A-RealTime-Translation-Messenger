ee27137304a352d2496453181d0018dee81a9f42
kgs19970120 <rmstn9007@naver.com>
2020-12-09 23:36:32 +0900

    [iOS] Feat : :zap:️ Add checkAnimationView at ViewController #282
    - 공동 사용을 위해 ViewController에 checkAnimationView 추가 완료

--- iOS/Pupago/Pupago/Common/ViewController.swift
+++ iOS/Pupago/Pupago/Common/ViewController.swift
@@ -6,11 +6,24 @@
 //
 
 import UIKit
+import Lottie
+import AudioToolbox
 
 class ViewController: UIViewController, Navigatable {
     
     var viewModel: ViewModel?
     var navigator: Navigator!
+    
+    lazy var checkAnimationView: AnimationView = {
+        let view = AnimationView(name: "check")
+        view.animationSpeed = 1.5
+        view.frame = CGRect(x: 0, y: 0, width: 120, height: 120)
+        view.center = self.view.center
+        view.contentMode = .scaleAspectFill
+        
+        self.view.addSubview(view)
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,4 +38,12 @@ class ViewController: UIViewController, Navigatable {
     func bindViewModel() {}
     func registerForKeyboardNotifications() {}
     
+}
+
+extension ViewController {
+    func playCheckSoundAndPause(for milliSecond: Int) {
+        let msMultiflier = 1000
+        AudioServicesPlaySystemSound(1407)
+        usleep(useconds_t(milliSecond * msMultiflier))
+    }
 }
