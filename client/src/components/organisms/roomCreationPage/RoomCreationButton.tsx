84c080389099ce2cef7a95eb2298879796c84d6a
kas136 <kas136@naver.com>
2020-12-09 23:16:52 +0900

    [Web] Feat : :zap:️ create organisms for RoomCreationPage #280
    RoomCreationPage에 필요한 기능 생성

---
+++ client/src/components/organisms/roomCreationPage/RoomCreationButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import useRoom from '../../../hooks/useRoom';
+
+import MainButton from '../../atoms/buttons/MainButton';
+
+const Wrapper = styled.div``;
+
+const validate = (title: string) => {
+  if (title.length < 2 || title.length > 30) return false;
+  return true;
+};
+
+function RoomCreationButton() {
+  //   const { nicknameData, imageLinkData } = useUser();
+  const { data: roomData, onCreateRoom } = useRoom();
+
+  return (
+    <>
+      <MainButton
+        disabled={!validate(roomData.title)}
+        onClickButton={() => {
+          onCreateRoom({ title: roomData.title, isPrivate: roomData.isPrivate });
+        }}
+      >
+        생성하기
+      </MainButton>
+    </>
+  );
+}
+
+export default RoomCreationButton;
