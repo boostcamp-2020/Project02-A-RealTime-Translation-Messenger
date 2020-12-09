ec69b0d748f5671072fa59b2411101c4a6c87349
kas136 <kas136@naver.com>
2020-12-09 23:16:28 +0900

    [Web] Fix : :fire: Fix molecule for RoomCreationPage #280
    RoomCreationPage를 만드는 도중에 필요한 기능 일부 수정

--- client/src/components/molecules/RoomCreationPage/RoomCreation.tsx
+++ client/src/components/molecules/RoomCreationPage/RoomCreation.tsx
@@ -11,19 +11,22 @@ export type RoomCreationPropsType = {
   MaxWordCount: number;
   privateSelected?: boolean;
   InputOnChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+  isPrivateOnClick: (isPrivate: 'true' | 'false') => void;
 };
 
 const RoomCreationWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 344px;
+  margin-bottom: 96px;
 `;
 
 const CheckBoxWrapper = styled.div`
   display: flex;
   justify-content: center;
   width: 400px;
-  margin-bottom: 32px;
+  margin-bottom: 56px;
 
   & > div {
     margin-right: 30px;
@@ -45,19 +48,30 @@ export const RoomCreation = ({
   MaxWordCount,
   privateSelected,
   InputOnChange,
+  value,
+  isPrivateOnClick,
 }: RoomCreationPropsType) => {
   return (
     <>
       <CheckBoxWrapper>
-        <CheckBox isChecked={privateSelected ? false : true}>공개</CheckBox>
-        <CheckBox isChecked={privateSelected ? true : false}>비공개</CheckBox>
+        <CheckBox isChecked={privateSelected ? false : true} isPrivateOnClick={isPrivateOnClick} isPrivate={'false'}>
+          공개
+        </CheckBox>
+        <CheckBox isChecked={privateSelected ? true : false} isPrivateOnClick={isPrivateOnClick} isPrivate={'true'}>
+          비공개
+        </CheckBox>
       </CheckBoxWrapper>
       <RoomCreationWrapper>
-        <UnderLinedInput placeholder={'방 제목을 입력해주세요.'} maxLength={30} onChange={InputOnChange} />
+        <UnderLinedInput
+          value={value}
+          placeholder={'방 제목을 입력해주세요.'}
+          maxLength={30}
+          onChange={InputOnChange}
+        />
         <RightAlignedText
           size={14}
           color={Palette.PUPAGO_BLUE}
-        >{`(${TypedWordCount} / ${MaxWordCount})`}</RightAlignedText>
+        >{`${TypedWordCount} / ${MaxWordCount}`}</RightAlignedText>
       </RoomCreationWrapper>
     </>
   );
