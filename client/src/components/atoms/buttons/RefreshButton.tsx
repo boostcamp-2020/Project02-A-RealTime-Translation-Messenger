76eac72e8afbda9c6c0b064da9b0a51c6419edd5
moaikang <kk_w1207@naver.com>
2020-12-10 14:40:02 +0900

    [Web] Fix : :fire: Fix RefreshButton's size #297
    RefreshButton의 사이즈를 조정할 수 있게 만들었다.

--- client/src/components/atoms/buttons/RefreshButton.tsx
+++ client/src/components/atoms/buttons/RefreshButton.tsx
@@ -6,22 +6,27 @@ import Palette from '../../../@types/Palette';
 
 export type RefreshButtonTypes = {
   onClickRefresh: () => void;
+  size: 'big' | 'small';
 };
 
-const IconWrapper = styled.div`
+const IconWrapper = styled.div<RefreshButtonStyleType>`
   display: flex;
   justify-content: center;
   align-items: center;
   position: absolute;
-  width: 344px;
+  width: ${(props) => (props.size === 'big' ? '344px' : '232px')};
   height: 32px;
   cursor: pointer;
 `;
 
-const RefrshButtonWrapper = styled.div`
-  width: 344px;
+type RefreshButtonStyleType = {
+  size: 'big' | 'small';
+};
+
+const RefrshButtonWrapper = styled.div<RefreshButtonStyleType>`
+  width: ${(props) => (props.size === 'big' ? '344px' : '232px')};
   height: 32px;
-  margin: 32px 0 16px 0;
+  margin: ${(props) => (props.size === 'big' ? '32px 0 16px 0;' : '0 0 16px 0')};
   border-radius: 5px;
   background-color: ${Palette.PUPAGO_BLUE};
 
@@ -30,10 +35,10 @@ const RefrshButtonWrapper = styled.div`
   }
 `;
 
-export function RefreshButton({ onClickRefresh }: RefreshButtonTypes) {
+export function RefreshButton({ onClickRefresh, size }: RefreshButtonTypes) {
   return (
-    <RefrshButtonWrapper onClick={onClickRefresh}>
-      <IconWrapper>
+    <RefrshButtonWrapper onClick={onClickRefresh} size={size}>
+      <IconWrapper size={size}>
         <RefreshIcon style={{ fontSize: 24, color: 'white' }} />
       </IconWrapper>
     </RefrshButtonWrapper>
