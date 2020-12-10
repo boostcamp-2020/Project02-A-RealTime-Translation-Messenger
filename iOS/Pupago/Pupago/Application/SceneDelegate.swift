5c16291eac29891e217c10d871bff0d3715d4b28
kgs19970120 <rmstn9007@naver.com>
2020-12-10 15:36:20 +0900

    [iOS] Feat : :zap:️Implement socket life cycle control logic #300

--- iOS/Pupago/Pupago/Application/SceneDelegate.swift
+++ iOS/Pupago/Pupago/Application/SceneDelegate.swift
@@ -6,15 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    let application = Application.shared
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        Application.shared.presentInitialScreen(in: window)
+        application.setUpSocket()
+        application.presentInitialScreen(in: window)
+        
+        var id: UInt32 = 0
+        let fileUrl = NSURL(string: "/System/Library/Audio/UISounds/acknowledgment_received.caf")!
+        AudioServicesCreateSystemSoundID(fileUrl, &id)
+        print(id)
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        SocketIOManager.shared.establishConnect()
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        application.isSocketValid.accept(false)
+        SocketIOManager.shared.closeConnection()
+    }
+    
 }
 
