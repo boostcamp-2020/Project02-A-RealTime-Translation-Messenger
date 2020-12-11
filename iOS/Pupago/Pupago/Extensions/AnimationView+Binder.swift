4aa609c413aaa2fc38e834ce53d3af7d0d6c78d5
kgs19970120 <rmstn9007@naver.com>
2020-12-11 10:36:31 +0900

    [iOS] Feat : :zap:️ mplement binder for animationView #320

--- iOS/Pupago/Pupago/Extensions/AnimationView+Binder.swift
+++ iOS/Pupago/Pupago/Extensions/AnimationView+Binder.swift
@@ -5,4 +5,17 @@
 //  Created by 김근수 on 2020/12/11.
 //
 
-import Foundation
+import Lottie
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: AnimationView {
+    
+    public var isOn: Binder<Bool> {
+        return Binder(self.base) { view, isOn in
+            view.isHidden = !isOn
+            isOn ? view.play() : view.stop()
+        }
+    }
+    
+}
