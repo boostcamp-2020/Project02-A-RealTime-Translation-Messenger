47e30d8ad0f622e8653572e4dade9136e1d445a7
kgs19970120 <rmstn9007@naver.com>
2020-12-11 10:39:59 +0900

    [iOS] Feat : :zap:️ mplement Scanning ViewModel, ViewController and Bind #320

--- iOS/Pupago/Pupago/Module/OCR/ScanningViewController.swift
+++ iOS/Pupago/Pupago/Module/OCR/ScanningViewController.swift
@@ -6,24 +6,56 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import VisionKit
 
 class ScanningViewController: ViewController {
-
+    
+    @IBOutlet weak var originTextView: UITextView!
+    @IBOutlet weak var translationTextView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var scanButton: UIButton!
+    @IBOutlet weak var dismissButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func bindViewModel() {
+        super.bindViewModel()
+        guard let viewModel = viewModel as? ScanningViewModel else { return }
+        
+        let input = ScanningViewModel.Input(originText: originTextView.rx.text.orEmpty.asObservable(),
+                                            scanButtonTap: scanButton.rx.tap.asObservable(),
+                                            sendButtonTap: sendButton.rx.tap.asObservable(),
+                                            dismissButtonTap: dismissButton.rx.tap.asObservable())
+        
+        let output = viewModel.transform(input)
+        
+        output.originText
+            .bind(to: originTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        output.translationViewState
+            .bind(to: translationTextView.rx.state)
+            .disposed(by: rx.disposeBag)
+            
+        output.needScanning
+            .emit(onNext: { [unowned self] vc in
+                present(vc, animated: true, completion: nil)
+            })
+            .disposed(by: rx.disposeBag)
+        
+        output.needAnimation
+            .bind(to: scanningAnimationView.rx.isOn)
+            .disposed(by: rx.disposeBag)
+        
+        output.dismiss
+            .emit(onNext: { [unowned self] in
+                navigator.dismiss(sender: self)
+            })
+            .disposed(by: rx.disposeBag)
     }
-    */
 
 }
--- iOS/Pupago/Pupago/Module/OCR/ScanningViewModel.swift
+++ iOS/Pupago/Pupago/Module/OCR/ScanningViewModel.swift
@@ -5,8 +5,95 @@
 //  Created by 김근수 on 2020/12/10.
 //
 
-import UIKit
+//import UIKit
+import RxSwift
+import RxCocoa
+import VisionKit
 
-class ScanningViewModel: ViewModel {
-
+class ScanningViewModel: ViewModel, ViewModelType {
+    
+    typealias TranslationViewState = (text: String, isHidden: Bool)
+    
+    struct Input {
+        let originText: Observable<String>
+        let scanButtonTap: Observable<Void>
+        let sendButtonTap: Observable<Void>
+        let dismissButtonTap: Observable<Void>
+    }
+    
+    struct Output {
+        let needScanning: Signal<VNDocumentCameraViewController>
+        let originText: Observable<String>
+        let translationViewState: Observable<TranslationViewState>
+        let needAnimation: Observable<Bool>
+        let dismiss: Signal<Void>
+    }
+    
+    let scanedImage = PublishRelay<UIImage>()
+    let translationViewState = PublishRelay<(text: String, isHidden: Bool)>()
+    let chatInfo = PublishRelay<Translator.Text>()
+    let animating = BehaviorRelay<Bool>(value: false)
+    let originText = PublishRelay<String>()
+    
+    func transform(_ input: Input) -> Output {
+        
+        let provider = PupagoAPI()
+        let socketManager = SocketIOManager.shared
+        let translator = Translator(provider: provider)
+        
+        input.originText
+            .distinctUntilChanged()
+            .debounce(.milliseconds(200), scheduler: MainScheduler.instance)
+            .flatMap { translator.translate(with: $0) }
+            .subscribe(onNext: { [unowned self] info in
+                let translatedText = info.lang == "Korean" ? info.english : info.korean
+                chatInfo.accept(info)
+                translationViewState.accept((translatedText, false))
+            })
+            .disposed(by: rx.disposeBag)
+        
+        input.originText
+            .subscribe(onNext: { [unowned self] text in
+                let status = text.isEmpty ? ("", true) : (localize.value.translating, false)
+                translationViewState.accept(status)
+            })
+            .disposed(by: rx.disposeBag)
+        
+        scanedImage
+            .flatMap { [unowned self] image -> Observable<OCRResponse> in
+                animating.accept(true)
+                return provider.ocr(data: image.base64, timestamp: DateManager.currentTimeMillis(), requestId: UUID().uuidString)
+            }
+            .subscribe(onNext: { [unowned self] result in
+                animating.accept(false)
+                originText.accept(result.concatedString)
+            }, onError: { error in
+                print(error)
+            })
+            .disposed(by: rx.disposeBag)
+        
+        input.sendButtonTap
+            .withLatestFrom(chatInfo)
+            .subscribe(onNext: { info in
+                socketManager.sendMessage(korean: info.korean,
+                                          english: info.english,
+                                          origin: info.lang)
+            })
+            .disposed(by: rx.disposeBag)
+        
+        let needScan = input.scanButtonTap.asSignal(onErrorJustReturn: ())
+            .map { [unowned self] () -> VNDocumentCameraViewController in
+                let cameraViewController = VNDocumentCameraViewController()
+                cameraViewController.rx.didFinish
+                    .bind(to: scanedImage)
+                    .disposed(by: rx.disposeBag)
+                return cameraViewController
+            }
+        
+        return Output(needScanning: needScan,
+                      originText: originText.asObservable(),
+                      translationViewState: translationViewState.asObservable(),
+                      needAnimation: animating.asObservable(),
+                      dismiss: input.dismissButtonTap.asSignal(onErrorJustReturn: ()))
+    }
 }
