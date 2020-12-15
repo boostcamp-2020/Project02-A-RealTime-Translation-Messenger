ef76c2dae9b686352569a0a9b9f52ae42b3ea081
kas136 <kas136@naver.com>
2020-12-15 11:21:51 +0900

    [Web] Feat : :zap:️ Apply https to server #380
    서버를 https로 변경하기 위해 fs, path 설치
    option을 지정하여 createServer 매개변수 변경

---
+++ server/src/loaders/option.ts
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+
+const { SERVER_FULLCHAIN, SERVER_PRIVKEY, SERVER_CERT } = process.env;
+
+const optionInitialize = {
+  ca: fs.readFileSync(SERVER_FULLCHAIN!),
+  key: fs.readFileSync(path.resolve(SERVER_PRIVKEY!), 'utf8').toString(),
+  cert: fs.readFileSync(path.resolve(process.cwd(), SERVER_CERT!), 'utf8').toString(),
+};
+
+export default optionInitialize;
