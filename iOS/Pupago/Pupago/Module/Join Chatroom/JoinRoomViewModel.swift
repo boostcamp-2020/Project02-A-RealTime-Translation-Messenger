683de955acafd48850694d13afe7c43e1ca03af0
kgs19970120 <rmstn9007@naver.com>
2020-12-15 14:34:58 +0900

    [iOS] Refactor: :rainbow: Update Room Join Moduel #384
    - 변수, 이벤트 네이밍 수정
    - Dimming view 터치 시 dismiss 로직 추가
    - input 로직 간략화

--- iOS/Pupago/Pupago/Module/Join Chatroom/JoinRoomViewModel.swift
+++ iOS/Pupago/Pupago/Module/Join Chatroom/JoinRoomViewModel.swift
@@ -5,57 +5,68 @@
 //  Created by 김근수 on 2020/11/25.
 //
 
-import Foundation
 import RxSwift
 import RxCocoa
 
 final class JoinRoomViewModel: ViewModel, ViewModelType {
     
+    // MARK: - Input
+    
     struct Input {
         let roomCode: Observable<[String]>
-        let joinTrigger: Observable<Void>
-        let cancelTrigger: Observable<Void>
+        let dimmingViewDidTap: Observable<Void>
+        let joinButtonDidTap: Observable<Void>
+        let closeButtonDidTap: Observable<Void>
     }
+    
+    // MARK: - Output
     
     struct Output {
         let viewTexts: Driver<Localize.JoinRoomViewText>
-        let activate: Driver<Bool>
-        let dismiss: Driver<Void>
+        let isActive: Driver<Bool>
+        let dismiss: Signal<Void>
     }
+    
+    // MARK: - State
     
     let isFull = BehaviorRelay<Bool>(value: false)
     let isValid = BehaviorRelay<Bool>(value: false)
     let roomInfo = PublishSubject<(code: String, isPrivate: Bool)>()
     
+    // MARK: - Transform
+    
     func transform(_ input: Input) -> Output {
         
         input.roomCode
+            .distinctUntilChanged()
             .map { $0.joined().uppercased() }
             .subscribe(onNext: { [unowned self] code in
-                self.isFull.accept(code.count == 4)
-                self.isValid.accept(self.validate(code))
+                print(code)
+                isFull.accept(code.count == 4)
+                isValid.accept(validate(code))
             })
             .disposed(by: rx.disposeBag)
         
-        input.joinTrigger
+        input.joinButtonDidTap
             .withLatestFrom(input.roomCode)
             .map { ($0.joined(), true) }
-            .bind(to: self.roomInfo)
+            .bind(to: roomInfo)
             .disposed(by: rx.disposeBag)
         
-        let viewText = localize.asDriver()
+        let viewTexts = localize.asDriver()
             .map { $0.joinRoomViewText }
         
         let activate = Observable.combineLatest(isFull, isValid)
             .map { $0 && $1 }
             .asDriver(onErrorJustReturn: false)
-
-        let dismiss = Observable.of(input.cancelTrigger, input.joinTrigger).merge()
-            .map { _ in }
-            .asDriver(onErrorJustReturn: ())
         
-        return Output(viewTexts: viewText,
-                      activate: activate,
+        let dismiss = Observable.of(input.closeButtonDidTap,
+                                    input.joinButtonDidTap,
+                                    input.dimmingViewDidTap).merge()
+            .asSignal(onErrorJustReturn: ())
+        
+        return Output(viewTexts: viewTexts,
+                      isActive: activate,
                       dismiss: dismiss)
     }
     
