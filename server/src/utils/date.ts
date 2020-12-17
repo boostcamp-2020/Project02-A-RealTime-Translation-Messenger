05e5abfa393d30bbdb2e33e61f04759100b1450e
kas136 <kas136@naver.com>
2020-12-17 14:28:06 +0900

    [Web] Fix : :fire: 버그 fix get date function #430
    linux 환경에서는 new Date()의 반환값의 포맷이 달라 일부 코드 수정

--- server/src/utils/date.ts
+++ server/src/utils/date.ts
@@ -4,16 +4,15 @@ const getNow = () => {
   const date = new Date().toLocaleDateString().split(Time.DEVIDE_DATE);
   const timeIncludeMeridiem = new Date().toLocaleTimeString().split(Time.DEVIDE_MERIDIEM);
   const time = timeIncludeMeridiem[0].split(Time.DEVIDE_TIME);
-  const meridiem = timeIncludeMeridiem[2]?.substring(0, 2);
 
+  if (date[0]?.length < 2) date[0] = Time.ADD_DEFAUTL_TIME + date[0];
   if (date[1]?.length < 2) date[1] = Time.ADD_DEFAUTL_TIME + date[1];
-  if (date[2]?.length < 2) date[2] = Time.ADD_DEFAUTL_TIME + date[2];
   time[0] =
-    time[0].length < 2 && meridiem === Time.STANDARD_MERIDIEM
+    time[0].length < 2 && timeIncludeMeridiem[1] === Time.STANDARD_MERIDIEM
       ? (time[0] = String(Time.STANDARD_TIME + Number(time[0])))
       : time[0];
 
-  const newDate = [date[0], date[1], date[2]].join(Time.DEVIDE_DATE);
+  const newDate = [date[2], date[0], date[1]].join(Time.DEVIDE_DATE);
   const newTime = [time[0], time[1], time[2]].join(Time.DEVIDE_TIME);
 
   return newDate + Time.DEVIDE_MERIDIEM + newTime;
